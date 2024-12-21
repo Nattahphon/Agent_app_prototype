6f6888448cb2cd30af119d74beea329477ffa1bf
Natthaphon Chai <natthaphon.chaichoom@gmail.com>
2024-12-21 20:13:36 +0700

    modify output to dict, json

--- H_summary_app.py
+++ H_summary_app.py
@@ -80,14 +80,14 @@ if __name__ == "__main__":
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."""
     data = sum_agent.summarize(text=text)
     # Decomposing and printing components
-    print("Summary:")
-    print(data['summary'])
-    print("\nNotes:")
-    for note in data['notes']:
-        for key, value in note.items():
-            print(f"  {key.capitalize()}: {value}")
+    print(data)
+    # print(data['summary'])
+    # print("\nNotes:")
+    # for note in data['notes']:
+    #     for key, value in note.items():
+    #         print(f"  {key.capitalize()}: {value}")
 
-    print("\nHighlights:")
-    for key, value in data['highlights'].items():
-        print(f"  {key.capitalize()}: {value}")
+    # print("\nHighlights:")
+    # for key, value in data['highlights'].items():
+    #     print(f"  {key.capitalize()}: {value}")
 
